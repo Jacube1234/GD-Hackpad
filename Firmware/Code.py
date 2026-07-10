c1641237a3fc98e717b21c40be24af2326687e11
Jacube <jacobasherocallaghan@gmail.com>
2026-07-10 12:45:45 +1200

    Added rotary knob
    Added rotary knob. Made skull smaller

--- Firmware/Code.py
+++ Firmware/Code.py
@@ -2,15 +2,16 @@ print("Starting")
 
 import board
 
+from kmk.extensions.RGB import RGB
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 
 keyboard = KMKKeyboard()
+keyboard.pins = (board.GP8,board.GP9,board.GP10,board.GP11)
 
-keyboard.col_pins = (board.GP0,)
-keyboard.row_pins = (board.GP1,)
-keyboard.diode_orientation = DiodeOrientation.COL2ROW
+rgb = RGB(pixel_pin=board.GP1, num_pixels=2)
+keyboard.extensions.append(rgb)
 
 keyboard.keymap = [
     [KC.UP, KC.DOWN, KC.LEFT, KC.RIGHT]
